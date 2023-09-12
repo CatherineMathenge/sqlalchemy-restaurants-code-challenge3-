5a98cc7dfbf95b490f15cb64a08f952a502f6263
Catherine <catherine.mathenge@strathmore.edu>
2023-09-12 23:39:27 +0300

    Done with assignment

---
+++ seeds.py
@@ -0,0 +1,22 @@
+# Add sample data (You can replace this with your own data)
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Restaurant, Customer, Review
+
+
+engine = create_engine('sqlite:///restaurant_reviews.db')
+Base.metadata.create_all(engine)
+session = sessionmaker(bind=engine)
+session = session()
+
+
+customer1 = Customer(first_name='Catherine', last_name='Mathenge')
+customer2 = Customer(first_name='Christine', last_name='Mathenge')
+restaurant1 = Restaurant(name='Restaurant A', price=4)
+restaurant2 = Restaurant(name='Restaurant B', price=5)
+review1 = Review(star_rating=4, restaurant=restaurant1, customer=customer1)
+review2 = Review(star_rating=5, restaurant=restaurant2, customer=customer2)
+
+session.add_all([customer1, customer2, restaurant1, restaurant2, review1, review2])
+session.commit()
+
