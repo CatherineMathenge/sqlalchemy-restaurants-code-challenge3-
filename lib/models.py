f4329df636d899828d0e6e268281e90ebf24c615
Catherine <catherine.mathenge@strathmore.edu>
2023-09-13 10:31:39 +0300

    Individual module for each class

--- lib/models.py
+++ lib/models.py
@@ -1,41 +1,16 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 # Create a SQLAlchemy database engine (SQLite in this example)
 engine = create_engine('sqlite:///restaurant_reviews.db')
 Base = declarative_base()
 
-#  Define the Restaurant model
-class Restaurant(Base):
-    __tablename__ = 'restaurants'
+# Import your models from separate modules
+from restaurant import Restaurant
+from customer import Customer
+from review import Review
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    price = Column(Integer)
-
-    # Define the relationship with Review
-    reviews = relationship('Review', back_populates='restaurant')
-
-# Define the Customer model
-class Customer(Base):
-    __tablename__ = 'customers'
-
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String)
-    last_name = Column(String)
- # Define the relationship with Review
-    reviews = relationship('Review', back_populates='customer')
-
-    # Define the Review model
-class Review(Base):
-    __tablename__ = 'reviews'
-
-    id = Column(Integer, primary_key=True)
-    star_rating = Column(Integer)
-    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
-    customer_id = Column(Integer, ForeignKey('customers.id'))
-
-    # Define the relationships with Restaurant and Customer
-    restaurant = relationship('Restaurant', back_populates='reviews')
-    customer = relationship('Customer', back_populates='reviews')
+# Create a session to interact with the database
+Session = sessionmaker(bind=engine)
+session = Session()
